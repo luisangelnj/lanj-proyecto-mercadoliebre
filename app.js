fc634ea9a48421e123fc8aa951fdf8ccc8e59938
luisangelnj <njluisangel@gmail.com>
2021-07-07 17:27:49 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
+
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor corriendo en el puerto 3000");
+})
